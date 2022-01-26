5d56e54dfe0922062b0764dd4da7882a9757667b
mdemon420 <mdeamon4@gmail.com>
2022-01-26 23:19:31 +0600

    dry code

--- script.js
+++ script.js
@@ -6,21 +6,34 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 
 const btnCloseModal = document.querySelector('.close-modal');
 
+const openModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const closeModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
 for(let i = 0; i < btnsShowModal.length; i++)
 
     btnsShowModal[i].addEventListener('click',function(){
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
+       closeModal();
     }); 
 
+  
+
     btnCloseModal.addEventListener('click',function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
+        // modal.classList.add('hidden');
+        // overlay.classList.add('hidden');
+        openModal();
     });
     overlay.addEventListener('click',function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
+        // modal.classList.add('hidden');
+        // overlay.classList.add('hidden');
+        openModal();
+    });
 
 
 
