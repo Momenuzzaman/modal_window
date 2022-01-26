ef0ed3c5eea98283c42a922cf33d81765d22cdc7
mdemon420 <mdeamon4@gmail.com>
2022-01-26 23:04:02 +0600

    colse Button add

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const overlay = document.querySelector('.overlay');
 
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-const btnCloseModal = document.querySelector('close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
-for(let i = 0; i < btnsShowModal.length; i++){
-    let btnShowModal = btnsShowModal[i];
-    
-    btnShowModal.addEventListener('click',function(){
+for(let i = 0; i < btnsShowModal.length; i++)
+
+    btnsShowModal[i].addEventListener('click',function(){
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
+    }); 
+
+    btnCloseModal.addEventListener('click',function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
     });
-    
-}
+
+
+
+
