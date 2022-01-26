9ac85a37fee31cd608c549f291a4e71029aac4c4
mdemon420 <mdeamon4@gmail.com>
2022-01-26 21:29:19 +0600

    all class selact

---
+++ script.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const btnCloseModal = document.querySelector('close-modal');
+
+for(let i = 0; i < btnsShowModal.length; i++)
+console.log(btnsShowModal[i]);
